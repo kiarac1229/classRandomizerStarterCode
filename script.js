28b32e1c01490ad4e7b0bbf2b13f48d3f4a1b1fa
Kiara Correa Acosta <kcorrea@aclanyc.com>
2017-05-03 12:35:41 +0000

    I changed stuff

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
 $(document).ready(function(){
+var teachers = ["Alyxe", "Julia", "Zack", "Aaron", "Justin"];
+
+var students = ["Luis", "Chris","Sammy", "Jessie", "Kiara", "Monserrat", "Julio", "Yanella", "Adonis", "Jen", "Leslie", "Zen", "David","Kevin"];
 
 
+$("#studentButton").click(function(){
+    var random_student = Math.floor(Math.random()*students.length);
+    $("#studentDisplay").html(students[random_student]);
+    
+});
 
+$("#teacherButton").click(function(){
+    var random_teacher = Math.floor(Math.random()*teachers.length);
+    $("#teacherDisplay").html(teachers[random_teacher]);
+    
+});
+    
 });
